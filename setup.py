d2ac881e9b915ed873f11bd53821e1b130e3ece8
lyhryn <lyr@DESKTOP-78OM0OV>
2021-12-12 10:01:30 +0100

    gaming

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Python meme package by lyryhn
+#            2021
+
+from setuptools import setup
+
+
+setup(
+    name='python',
+    version='0.1.0',
+    description='Python Meme Package',
+    url='https://github.com/lyhryn/python-py',
+    author='lyryhn',
+    author_email='donttrytodoxme@gmail.com',
+    packages=['python'],
+    
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',   
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
